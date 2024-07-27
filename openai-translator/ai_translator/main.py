295296eb1f5f4736272da54f722eba9de40dc541
AnnaZhao <zhaoan7@oocl.com>
2024-07-27 21:51:27 +0800

    openai-translator: with multiple languages

--- openai-translator/ai_translator/main.py
+++ openai-translator/ai_translator/main.py
@@ -1,6 +1,9 @@
 import sys
 import os
 
+os.environ['OPENAI_API_KEY'] = 'sk-6KpfygRF1Nm5uEW5A0756aDeC4114dA5Af7b7e518e5230Eb'
+os.environ['OPENAI_BASE_URL'] = 'https://api.xiaoai.plus/v1'
+
 sys.path.append(os.path.dirname(os.path.abspath(__file__)))
 
 from utils import ArgumentParser, ConfigLoader, LOG
@@ -16,6 +19,8 @@ if __name__ == "__main__":
 
     model_name = args.openai_model if args.openai_model else config['OpenAIModel']['model']
     api_key = args.openai_api_key if args.openai_api_key else config['OpenAIModel']['api_key']
+    # source_language = args.source_language if args.source_language else 'English'
+    target_language = args.target_language if args.target_language else '中文'
     model = OpenAIModel(model=model_name, api_key=api_key)
 
 
@@ -24,4 +29,4 @@ if __name__ == "__main__":
 
     # 实例化 PDFTranslator 类，并调用 translate_pdf() 方法
     translator = PDFTranslator(model)
-    translator.translate_pdf(pdf_file_path, file_format)
+    translator.translate_pdf(pdf_file_path, file_format, target_language)
